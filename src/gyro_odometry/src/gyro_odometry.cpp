23cbe83506eaaae21fbe18ce49e024c7be7ae33a
JoySt1ck <1316224681@qq.com>
2024-03-16 20:49:40 +0800

    odom fixed

--- src/gyro_odometry/src/gyro_odometry.cpp
+++ src/gyro_odometry/src/gyro_odometry.cpp
@@ -4,20 +4,23 @@
 #include <cstring>
 #include <iostream>
 
+bool serialopen = 1; //when serial is open, change it to 1, otherwise, change it to 0;
 float data_value[13];
 class TwistWithCovarianceNode : public rclcpp::Node
 {
 public:
 
     // Replace "/dev/ttyUSB0" with the actual serial port name
-    TwistWithCovarianceNode() : Node("TwistWithCovariance"), serial_port_("/dev/ttyACM0", 115200, serial::Timeout::simpleTimeout(1000))
+    TwistWithCovarianceNode() : Node("twist_with_covariance"), serial_port_("/dev/ttyACM0", 115200, serial::Timeout::simpleTimeout(1000))
     {
-        publisher_ = this->create_publisher<geometry_msgs::msg::TwistWithCovarianceStamped>("TwistWithCovariance", 1000/period_ms);
+        publisher_ = this->create_publisher<geometry_msgs::msg::TwistWithCovarianceStamped>("twist_with_covariance", 1000/period_ms);
         
         timer_ = this->create_wall_timer(std::chrono::milliseconds(period_ms), std::bind(&TwistWithCovarianceNode::serialCallback, this));
     }
 
+
 private:
+   
     void serialCallback()
     {
         if (serial_port_.available() > 0)
@@ -30,11 +33,11 @@ private:
             std::string tmp = "";
 
             for (int i = 0; i < len; i++) {
-                if (data[i] == ' ' || data[i] == '\n') {
-                    // 跳过空格和换行符
+                if (data[i] == ' ') {
+                    // 跳过空格
                     continue;
                 }
-                if (data[i] == ',') {
+                if (data[i] == ',' || data[i] == '\n') {
                     // 将 tmp 转换为浮点数并存储到 data_value 数组中
                     try {
                         data_value[tot++] = std::stof(tmp);
@@ -48,14 +51,10 @@ private:
                 }
             }
 
-            // // 检查是否成功解析了13个值
-            // if (tot != 12) {
-            //     std::cerr << "Error: Expected 13 values, but found " << tot << std::endl;
-            // } 
-            // std::cout << data << std::endl;
+
 
             msg.header.stamp = rclcpp::Clock{}.now();
-            msg.header.frame_id = "TwistWithCovariance";
+            msg.header.frame_id = "twist_with_covariance";
             msg.twist.twist.linear.x = data_value[7];
             msg.twist.twist.linear.y = data_value[8];
             msg.twist.twist.linear.z = data_value[9];
@@ -72,11 +71,50 @@ private:
     int period_ms = 20; // pubilish period
 };
 
+class emptytwistNode : public rclcpp::Node
+{
+public:
+    emptytwistNode() : Node("twist_with_covariance")
+    {
+        publisher_ = this->create_publisher<geometry_msgs::msg::TwistWithCovarianceStamped>("twist_with_covariance", 1000/period_ms);
+        
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(period_ms), std::bind(&emptytwistNode::publishEmptyMessage, this));
+    }
+
+
+private:
+   
+    void publishEmptyMessage() {
+        geometry_msgs::msg::TwistWithCovarianceStamped empty_twist_msg;
+        // Fill the message with default values or leave it empty depending on your application
+        empty_twist_msg.header.stamp = rclcpp::Clock{}.now();
+        empty_twist_msg.header.frame_id = "twist_with_covariance";
+        empty_twist_msg.twist.twist.linear.x = 0;
+        empty_twist_msg.twist.twist.linear.y = 0;
+        empty_twist_msg.twist.twist.linear.z = 0;
+        empty_twist_msg.twist.twist.angular.x = 0;
+        empty_twist_msg.twist.twist.angular.y = 0;
+        empty_twist_msg.twist.twist.angular.z = 0;
+        publisher_->publish(empty_twist_msg);
+    }
+    rclcpp::Publisher<geometry_msgs::msg::TwistWithCovarianceStamped>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    int period_ms = 20; // pubilish period
+};
+
+
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<TwistWithCovarianceNode>();
-    rclcpp::spin(node);
+    
+ 
+    if (serialopen){
+        auto node = std::make_shared<TwistWithCovarianceNode>();
+        rclcpp::spin(node);
+    } else {
+        auto emptynode = std::make_shared<emptytwistNode>();
+        rclcpp::spin(emptynode);
+    }
     rclcpp::shutdown();
     return 0;
 }
